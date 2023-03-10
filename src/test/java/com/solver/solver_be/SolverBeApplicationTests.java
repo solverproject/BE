bbfa552aabf68a142936e1669b70c3d32010dde2
이승렬 <ibban9810@icloud.com>
2023-03-10 17:41:16 +0900

    Feat: Init Commit

---
+++ src/test/java/com/solver/solver_be/SolverBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solver.solver_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolverBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
