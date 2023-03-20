e377066c7dc0b9f7043f3f158eef3937cb585eff
LEE <96409909+LEESEUNGRYEOL@users.noreply.github.com>
2023-03-20 22:07:10 +0900

    Feat: Delete Algorithm 수정 #34 (#40)
    1. 재귀함수로 전체 내역들 삭제

--- src/main/java/com/solver/solver_be/domain/board/questionBoard/repository/QuestionBoardRepository.java
+++ src/main/java/com/solver/solver_be/domain/board/questionBoard/repository/QuestionBoardRepository.java
@@ -4,7 +4,9 @@ import com.solver.solver_be.domain.board.questionBoard.entity.QuestionBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuestionBoardRepository extends JpaRepository<QuestionBoard, Long> {
     List<QuestionBoard> findAllByOrderByCreatedAtDesc();
+    Optional<List<QuestionBoard>> findByParentBoardId(Long id);
 }
