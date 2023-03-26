3474fbbda46c396ecac08efadd05df98b1de3ab3
Ganpyeon <jms03096@naver.com>
2023-03-27 00:39:02 +0900

    Fix: 코드 전반적 수정 #15
    1. ResponseCode에서 안쓰는 코드들 삭제
    2. Visitor -> VisitForm으로 변경
    3. Repository에서 안쓰는 method삭제
    4. CompanyService에 Transactioanl추가

--- src/main/java/com/solver/solver_be/global/security/refreshtoken/RefreshTokenRepository.java
+++ src/main/java/com/solver/solver_be/global/security/refreshtoken/RefreshTokenRepository.java
@@ -1,6 +1,5 @@
 package com.solver.solver_be.global.security.refreshtoken;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -8,6 +7,4 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findAllByUserEmail(String userEmail);
-
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
