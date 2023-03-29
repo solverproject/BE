aa9f2f5516a5c0cb20adb9bcedbfd9fff092b759
skwwnl <skwwnl@gmail.com>
2023-03-29 18:18:29 +0900

    Refactor : Basic Code Refactoring bug #15
    1. VisitFormService 방문신청 없을 경우에도 에러가 아닌 성공으로 반환.

--- src/main/java/com/solver/solver_be/domain/visitform/service/VisitFormService.java
+++ src/main/java/com/solver/solver_be/domain/visitform/service/VisitFormService.java
@@ -52,9 +52,9 @@ public class VisitFormService {
 
         List<VisitForm> visiFormUserList = visitFormRepository.findByGuestId(guest.getId());
 
-        if (visiFormUserList.isEmpty()) {
-            throw new VisitFormException(ResponseCode.VISITOR_NOT_FOUND);
-        }
+//        if (visiFormUserList.isEmpty()) {
+//            throw new VisitFormException(ResponseCode.VISITOR_NOT_FOUND);
+//        }
 
         List<VisitFromResponseDto> visitFromResponseDtoList = new ArrayList<>();
         for (VisitForm visitorForm : visiFormUserList) {
