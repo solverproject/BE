1f408ecb47755df77946a84aac7389bcd069109e
Ganpyeon <jms03096@naver.com>
2023-03-31 16:58:34 +0900

    Refactor: Querydsl 수정 #15
    1. builder.and -> builder,or로 변경

--- src/main/java/com/solver/solver_be/domain/visitform/repository/CustomVisitFormRepository.java
+++ src/main/java/com/solver/solver_be/domain/visitform/repository/CustomVisitFormRepository.java
@@ -5,5 +5,5 @@ import com.solver.solver_be.domain.visitform.entity.VisitForm;
 import java.util.List;
 
 public interface CustomVisitFormRepository {
-    List<VisitForm> findByGuestNameAndLocationAndTargetAndStartDateAndEndDateAndPurposeAndStatus(String guestName, String location, String target, String startDate, String endDate, String purpose, String status);
+    List<VisitForm> findByGuestNameOrLocationOrTargetOrStartDateOrEndDateOrPurposeAndStatus(String guestName, String location, String target, String startDate, String endDate, String purpose, String status);
 }
